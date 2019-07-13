64117333b0b816cf9222e4828402872ce454ca36
zixujing <zixujing@126.com>
2019-07-14 02:36:10 +0800

    20190714

--- src/main/java/com/trans/mapper/DBMapper.java
+++
@@ -1,9 +0,0 @@
-package com.trans.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.trans.entity.DB;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBMapper extends BaseMapper<DB> {
-}
